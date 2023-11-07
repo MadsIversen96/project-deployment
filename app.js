fcd582a30a5c87a0a3421a4b1a6caf487353a35c
MadsIversen96 <madsmiversen@hotmail.no>
2023-11-07 14:22:14 +0100

    Lesson 1

--- app.js
+++ app.js
@@ -5,24 +5,29 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var picturesRouter = require('./routes/pictures');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const fileUpload = require('express-fileupload');
+
+app.use(fileUpload());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'pictures')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/pictures', picturesRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to the error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
